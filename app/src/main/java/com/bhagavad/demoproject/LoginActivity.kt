3592ed48a590155a637a24da3ce7c2002bcc6f50
PradeepClecotech <pradeep.jaiswal@clecotech.com>
2022-02-02 19:16:13 +0530

    Continue working on advance Login screen

--- app/src/main/java/com/bhagavad/demoproject/LoginActivity.kt
+++ app/src/main/java/com/bhagavad/demoproject/LoginActivity.kt
@@ -2,10 +2,18 @@ package com.bhagavad.demoproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        if (getSupportActionBar() != null) {
+            getSupportActionBar()?.hide();
+        }
+
+
     }
 }
