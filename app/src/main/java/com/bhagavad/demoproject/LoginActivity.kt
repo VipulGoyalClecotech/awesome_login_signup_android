bab8442d24a097f385210f4d7d8ac94ec0f29940
PradeepClecotech <pradeep.jaiswal@clecotech.com>
2022-02-02 20:55:16 +0530

    Continue working on signup screen

--- app/src/main/java/com/bhagavad/demoproject/LoginActivity.kt
+++ app/src/main/java/com/bhagavad/demoproject/LoginActivity.kt
@@ -1,19 +1,31 @@
 package com.bhagavad.demoproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.TextView
+//import androidx.databinding.DataBindingUtil
+//import com.bhagavad.demoproject.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-
+    //lateinit var mBinding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         if (getSupportActionBar() != null) {
             getSupportActionBar()?.hide();
         }
+     /*   mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
 
+        if (getSupportActionBar() != null) {
+            getSupportActionBar()?.hide();
+        }
+        mBinding.tvSignup.setOnClickListener {
+            val mIntent = Intent(this, SignupActivity::class.java)
+            startActivity(mIntent)
+
+        }*/
 
     }
 }
--- app/src/main/java/com/bhagavad/demoproject/SignupActivity.kt
+++ app/src/main/java/com/bhagavad/demoproject/SignupActivity.kt
@@ -1,11 +1,29 @@
 package com.bhagavad.demoproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import androidx.databinding.DataBindingUtil
+//import com.bhagavad.demoproject.databinding.ActivityLoginBinding
+//import com.bhagavad.demoproject.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+   // lateinit var mBinding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+       if (getSupportActionBar() != null) {
+           getSupportActionBar()?.hide();
+       }
+      /*  mBinding = DataBindingUtil.setContentView(this, R.layout.activity_signup);
+        mBinding.tvLogin.setOnClickListener {
+          finish()
+
+        }
+        mBinding.ivBack.setOnClickListener {
+            finish()
+
+        }*/
+
     }
 }
